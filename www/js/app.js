adeb7ef8069aba3308b86867e0914d2de0b118ca
Niharika Saluja <saluja.niharika@gmail.com>
2014-11-07 19:14:46 +0530

    User Account Page

--- www/js/app.js
+++ www/js/app.js
@@ -4,19 +4,7 @@ var artApp = angular.module('art', ['ui.router','angular-loading-bar', 'ngAnimat
 artApp.config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider, $httpProvider) {
     
     localStorageServiceProvider.setPrefix('artonline');
-    $httpProvider.responseInterceptors.push(["$q", "$timeout", function ($q, $timeout) {
-    return function (promise) {
-        var defer = $q.defer();
 
-        $timeout(function () {
-            promise.then(function (data) {
-                defer.resolve(data);
-            });
-        }, 2500);
-
-        return defer.promise;
-    };
-}]);
   // localStorageServiceProvider.setStorageCookieDomain('example.com');
   // localStorageServiceProvider.setStorageType('sessionStorage');
     $urlRouterProvider.otherwise('/home');
@@ -38,13 +26,20 @@ artApp.config(function($stateProvider, $urlRouterProvider, localStorageServicePr
         })
         .state('account',{
             url:'/account',
-            templateUrl:'templates/account.html'
+            templateUrl:'templates/account.html',
+            controller: 'AccountCtrl'
         })
         .state('collections',{
             url:'/collections',
             templateUrl:'templates/collections.html'
         });
         
+}).directive('stopClick', function() {
+    return function(scope, element, attrs) {
+        $(element).click(function(event) {
+            event.preventDefault();
+        });
+    }
 });
 
 
